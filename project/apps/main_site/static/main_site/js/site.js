8dbf352f89cb1000d4e9c85035e4ab1150e5e2ea
Steven Skoczen <steven@quantumimagery.com>
2012-04-06 10:44:37 -0700

    tweaks to timing and words in tulip, ready for imhana review

--- project/apps/main_site/static/main_site/js/site.js
+++ project/apps/main_site/static/main_site/js/site.js
@@ -18,17 +18,25 @@ var tulip_scroll_timeout = false;
 var tulip_scroll_top_cutoff = 0;
 var tulip_scroll_bottom_cutoff = 0;
 var TULIP_SCROLL_INTERVAL_TIME = 50; //ms
+var TUILP_FADEOUT_INCREMENT = 200; // ms
 
 $(function(){
 	resized();
 	$("h1.title").css("margin-top",win_height/2 - $("h1.title").height()/2);
-	$("poem").css("top",win_height/2 - $("poem").height()/2).css({"left":win_width/2 - $("poem").width()/2});
+	$("poem").css("top",-1000).css({"left":win_width/2 - $("poem").width()/2});
 	animate_action("main_delay_counter", "h1.title", "fadeIn(1000*ANIMATION_CONSTANT)", 4000);
 	animate_action("main_delay_counter", "h1.title", "fadeOut(1000*ANIMATION_CONSTANT)", 2000);
+
+	$("poem").delay(6000).css("top",win_height/2 - $("poem").height()/2);
 	make_visible_and_animate_in("main_delay_counter", "poem word.tulips", 1000, 1000);
 	make_visible_and_animate_in("main_delay_counter", "poem word.red", 1000, 2000);
 	make_visible_and_animate_in("main_delay_counter", "poem word.softly", 1000, 1000);
 	make_visible_and_animate_in("main_delay_counter", "poem word.wilted", 1000, 1000);
+	setTimeout(bind_mouse_actions, 11000);
+	
+});
+
+function bind_mouse_actions() {
 	$("poem word").click(poem_word_clicked);
 	// $("poem").bind("mouseover", poem_moused_over);
 	$("poem word").bind("mouseover", poem_word_moused_over);
@@ -38,11 +46,11 @@ $(function(){
 	$("#tulips_explosion word:not(.center)").bind("mouseover", tulips_mouseover);
 	$("#tulips_explosion word:not(.center)").bind("mouseout", tulips_mouseout);
 	$("#wilted_explosion line").bind("mouseover", wilted_line_hovered);
-});
+}
 
 function animate_action(counter_name, target, action, wait_after) {
 	// ugh, eval
-	console.log("$(\"" + target + "\").delay("+counter_name+ "*ANIMATION_CONSTANT)." + action + ";")
+	// console.log("$(\"" + target + "\").delay("+counter_name+ "*ANIMATION_CONSTANT)." + action + ";")
 	eval("$(\"" + target + "\").delay("+counter_name+ "*ANIMATION_CONSTANT)." + action + ";")
 	if (wait_after != undefined) {
 		eval(counter_name + "+= wait_after*ANIMATION_CONSTANT;");
@@ -50,7 +58,7 @@ function animate_action(counter_name, target, action, wait_after) {
 }
 
 function make_visible_and_animate_in(counter_name, target, animation_time, post_delay) {
-	animate_action(counter_name, target, "css({opacity: 0.0, visibility: 'visible'}).animate({opacity: 0.7}, "+animation_time*ANIMATION_CONSTANT+")", post_delay*ANIMATION_CONSTANT);
+	animate_action(counter_name, target, "css({opacity: 0.0, visibility: 'visible'}).show().animate({opacity: 0.7}, "+animation_time*ANIMATION_CONSTANT+")", post_delay*ANIMATION_CONSTANT);
 }
 
 function resized() {
@@ -78,20 +86,20 @@ function poem_word_clicked() {
 					red_end();
 					break;
 
-				// case "softly":
-				// 	$("poem").animate({"top":-35, "left": win_width-75}, 2000*ANIMATION_CONSTANT);	
-				// 	$("explosion#"+id+"_explosion").css({'opacity': 0}).show().animate({'opacity': 1}, 2000*ANIMATION_CONSTANT).addClass("current");
-				// 	break;
-
-				// case "wilted":
-				// 	$("poem").animate({"top":-35, "left": -70}, 2000*ANIMATION_CONSTANT);	
-				// 	$("explosion#"+id+"_explosion").css({'opacity': 0}).show().animate({'opacity': 1}, 2000*ANIMATION_CONSTANT).addClass("current");
-				// 	break;
-
-				default:
+				case "softly":
 					$("poem").animate({"top":win_height/2 - $("poem").height()/2, "left": win_width/2 - $("poem").width()/2}, 2000*ANIMATION_CONSTANT);	
 					$("explosion.current").animate({'opacity': 0}, 2000*ANIMATION_CONSTANT);
 					setTimeout(clear_main_animation_flag, 2000*ANIMATION_CONSTANT);
+					break;
+
+				case "wilted":
+					$("poem").delay(1000).animate({"top":win_height/2 - $("poem").height()/2, "left": win_width/2 - $("poem").width()/2}, 2000*ANIMATION_CONSTANT);	
+					$("explosion.current").animate({'opacity': 0}, 2000*ANIMATION_CONSTANT);
+					setTimeout(clear_main_animation_flag, 2000*ANIMATION_CONSTANT);
+					break;
+
+				default:
+					break;
 			}
 			zoomed_in = false;
 		} else {
@@ -158,9 +166,9 @@ function poem_moused_over() {
 }
 function poem_word_moused_over() {
 	var word = $(this);
-	word.stop().animate({'opacity': 0.9}, 200*ANIMATION_CONSTANT);
+	word.stop().animate({'opacity': 0.9}, 600*ANIMATION_CONSTANT);
 
-	$("poem word:not(." + word.attr("class") + ")").stop().animate({'opacity': 0.4}, 200*ANIMATION_CONSTANT);
+	$("poem word:not(." + word.attr("class") + ")").stop().animate({'opacity': 0.4}, 600*ANIMATION_CONSTANT);
 }
 
 function poem_moused_out() {
@@ -193,7 +201,7 @@ function tulips_mouseout() {
 
 function tulip_mousemoved(e) {
 	// if (e.pageY < tulip_scroll_top_cutoff || e.pageY > tulip_scroll_bottom_cutoff ) {
-		tulip_scroll_speed = (e.pageX / win_width) - 0.5;
+		tulip_scroll_speed = (e.pageX / win_width) - 0.4;
 		if ( Math.abs(tulip_scroll_speed) < 0.05 ) {
 			tulip_scroll_speed = 0;
 		} else {
@@ -216,13 +224,13 @@ function tulip_mousemoved(e) {
 function start_tulips() {
 	tulip_scroll_speed = 0.5;
 	$(window).bind("mousemove.tulips", tulip_mousemoved);
-	$("#tulips_explosion word").css({'margin-top': tulip_scroll_top_cutoff})
-
 
 	$("explosion#tulips_explosion")
 		.css({'opacity': 0, 'width':win_width, 'height':win_height, 'display': 'block', })
 		.animate({scrollLeft: 0}, 0);
-	
+	$("#tulips_explosion word").css({'margin-top': tulip_scroll_top_cutoff, 'opacity':1});
+
+
 	soil_1_initial_position = $("#soil_1").position().left;
 	soil_2_initial_position = $("#soil_word_2").position().left;
 	$("explosion#tulips_explosion")
@@ -230,7 +238,7 @@ function start_tulips() {
 		.animate({'opacity': 1}, 2000*ANIMATION_CONSTANT)
 		.addClass("current");
 
-	setTimeout(start_tulip_scroll, 1000);
+	setTimeout(start_tulip_scroll, 2050);
 }
 
 function start_tulip_scroll() {
@@ -251,7 +259,7 @@ function tulipscroll() {
 	}
 	
 
-	var speed =  (tulip_scroll_speed * 20);
+	var speed =  (tulip_scroll_speed * 16);
 	
 	var scroll_string = "+="+ speed;
 	$("#tulips_explosion").stop().animate({
@@ -259,12 +267,41 @@ function tulipscroll() {
 	}, TULIP_SCROLL_INTERVAL_TIME, "linear");	
 }
 
+function animate_out_word(word){
+	var opacity = (word.css('opacity')*1.0)-0.1;
+	word.css('opacity', opacity);
+	if (opacity > 0) {
+		setTimeout(function(){animate_out_word(word)}, 100);	
+	} else {
+		word.css('opacity', 0);	
+	}
+	
+}
+
 function end_tulips() {
-	$(window).unbind("mousemove.tulips");
-	$("poem").animate({"top":win_height/2 - $("poem").height()/2, "left": win_width/2 - $("poem").width()/2}, 2000*ANIMATION_CONSTANT);	
-	setTimeout(clear_main_animation_flag, 2000*ANIMATION_CONSTANT);
-	clearInterval(tulip_scroll_timeout)
-	$("#tulips_explosion").stop().animate({'opacity': 0}, 2000*ANIMATION_CONSTANT);
+	var timeout_counter = 0;
+	$("#tulips_explosion word").each(function(){
+		var word = $(this);
+		var left = word.offset().left;
+		if (left > -100 && left < win_width*1.2) {
+			setTimeout(function(){
+				animate_out_word(word);
+			}, timeout_counter);	
+			timeout_counter += TUILP_FADEOUT_INCREMENT*ANIMATION_CONSTANT;
+			// console.log(timeout_counter);
+		} else {
+			word.css({'opacity':0});
+		}
+	});
+	timeout_counter += 1400*ANIMATION_CONSTANT;
+
+	setTimeout(function(){
+		clearInterval(tulip_scroll_timeout);
+		clear_main_animation_flag();
+		$(window).unbind("mousemove.tulips");
+		$("poem").animate({"top":win_height/2 - $("poem").height()/2, "left": win_width/2 - $("poem").width()/2}, 2000*ANIMATION_CONSTANT);	
+		$("#tulips_explosion").hide();
+	}, timeout_counter);
 }
 
 function red_start() {
@@ -370,11 +407,11 @@ function softly_start() {
 					"css({'top':win_height*0.6, 'left': 0.3*win_width, 'opacity': 0}).show().animate({'opacity': 1.0, 'left':win_width*0.375}, 4000*ANIMATION_CONSTANT, 'linear').animate({'left':win_width*0.6, 'opacity':0}, 12000*ANIMATION_CONSTANT, 'linear')", 16000*ANIMATION_CONSTANT);
 	
 	animate_action("softly_delay_counter", "#softly_explosion line.oak", 
-					"css({'top':win_height*0.7, 'left': win_width*0.7, 'opacity': 0}).show().animate({'opacity': 1.0, 'left':0.557*win_width}, 4000*ANIMATION_CONSTANT, 'linear').animate({'left':win_width*0.2, 'opacity':0}, 10000*ANIMATION_CONSTANT, 'linear')", 13000*ANIMATION_CONSTANT);
+					"css({'top':win_height*0.7, 'left': win_width*0.7, 'opacity': 0}).show().animate({'opacity': 1.0, 'left':0.584*win_width}, 4000*ANIMATION_CONSTANT, 'linear').animate({'left':win_width*0.2, 'opacity':0}, 13000*ANIMATION_CONSTANT, 'linear')", 16000*ANIMATION_CONSTANT);
 	
 	// TODO: slow these down a teeny bit.
 	animate_action("softly_delay_counter", "#softly_explosion line.still", 
-					"css({'top':win_height*0.8, 'left': win_width*0.75, 'opacity': 0}).show().animate({'opacity': 1.0, 'left':0.6916*win_width}, 2000*ANIMATION_CONSTANT, 'linear').animate({'left':win_width*0.4, 'opacity':0}, 10000*ANIMATION_CONSTANT, 'linear')", 12000*ANIMATION_CONSTANT);
+					"css({'top':win_height*0.8, 'left': win_width*0.75, 'opacity': 0}).show().animate({'opacity': 1.0, 'left':0.7033*win_width}, 2000*ANIMATION_CONSTANT, 'linear').animate({'left':win_width*0.4, 'opacity':0}, 13000*ANIMATION_CONSTANT, 'linear')", 15000*ANIMATION_CONSTANT);
 	
 	animate_action("softly_delay_counter", "#softly_explosion line.shh2", 
 					"css({'top':win_height*0.7, 'left': win_width*0.7}).show().animate({'opacity': 1.0, 'top':win_height*0.566}, 6000*ANIMATION_CONSTANT, 'linear').animate({'top':0.3*win_height, 'opacity':0}, 12000*ANIMATION_CONSTANT, 'linear')", 18000*ANIMATION_CONSTANT);
